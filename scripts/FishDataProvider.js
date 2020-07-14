60a9e125f071635cd025b4fb8087d79c17efb4a0
mswinkels09 <mswinkels09@gmail.com>
2020-07-14 15:42:15 -0500

    finished adding automated fish to DOM

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -6,7 +6,7 @@ const fishCollection = [
         location: "Indo-Pacific Ocean",
         diet: "Sponges and Turnicates",
         image: "https://qph.fs.quoracdn.net/main-qimg-1443196e4d8a4e17176d6b97eedc4f37"
-    }
+    },
     {
         name: "Cleo",
         species: "Goldfish",
@@ -14,13 +14,17 @@ const fishCollection = [
         location: "Atlantic Ocean",
         diet: "algae",
         image: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Katri.jpg"
-    }
+    },
     {
         name: "Mrs. Puff",
         species: "Pufferfish",
-        length: "2",
+        length: 2,
         location: "Pacific Ocean",
         diet: "algae and invertebrates",
         image: "https://media.wsimag.com/attachments/716f6066bcc6b29e1054ca0264022c56d729607c/store/fill/1090/613/e6101c269024548aa40ddcac9b3fa54eee13cb721e7c8f8bcdf890436959/A-puffer-fish.jpg"
     }
 ]
+
+export const useFish = () => { 
+    return fishCollection.slice()
+}
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import {FishList} from "./FishList.js"
+
+FishList()
+
+// for (const fish of allTheFish){
+//     console.log(fish)
+// }
